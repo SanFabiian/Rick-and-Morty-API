7377387d5cdd1e2c4b5b852ec79535774cd3981f
SanFabian <sanfabiian@gmail.com>
2020-10-13 15:22:06 -0500

    rapa de deploy

--- src/pages/error404.js
+++
@@ -1,8 +0,0 @@
-const Error404 = () => {
-    const view = `
-    <div class="error404">
-        <h2>Error no se encontro el personaje</h2>
-    </div>`
-    return view
-}
-export default Error404
--- src/utils/getHash.js
+++
@@ -1,3 +0,0 @@
-const GetHash = () =>
-    location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/'
-export default GetHash
